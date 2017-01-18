beb57f57e8ecc35af9e0295770b14da9e75dbb06
vrnithinkumar <vrnithinkumar@gmail.com>
2017-01-18 14:43:17 +0530

    Statge

---
+++ FSharp Tuts/StartingEp002.fs
@@ -0,0 +1,30 @@
+type Details = 
+    {
+        Name: string
+        Description:string
+    }
+type Item =
+    {
+        Details: Details
+    }
+    
+
+type Exit =
+    | PassableExit of Details * destination: Room
+    | LockedExit of Details * key:Item
+    | NoExit
+
+type Exits =
+    {   
+        North: Exit
+        South: Exit
+        West : Exit
+        East : Exit
+    }
+
+
+type Room =
+    {
+        Details: Details 
+        Exits: Exits
+    }
