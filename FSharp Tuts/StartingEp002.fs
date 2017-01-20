93cd720248050f290f6f8b1aac964bd7093314d6
vrnithinkumar <vrnithinkumar@gmail.com>
2017-01-20 14:53:20 +0530

    Episode 3

--- FSharp Tuts/StartingEp002.fs
+++ FSharp Tuts/StartingEp002.fs
@@ -1,19 +1,21 @@
 type Details = 
     {
         Name: string
-        Description:string
-    }
-type Item =
-    {
-        Details: Details
+        Description: string
     }
 
+type Item =
+    { Details: Details }
+
+type RoomId = 
+    | RoomId of string
+
 type Exit =
-    | PassableExit of Details * destination: Room
+    | PassableExit of Details * destination: RoomId
     | LockedExit of Details * key:Item *next:Exit
     | NoExit of Details option
 
-and Exits =
+type Exits =
     {   
         North: Exit
         South: Exit
@@ -21,22 +23,43 @@ and Exits =
         East : Exit
     }
 
-and Room =
+type Room =
     {
+        Id: RoomId
         Details: Details 
         Items: Item list
         Exits: Exits
     }
 
-let firstRoom =
+type Player = 
     {
-        Details = {Name = "first Room "; Description = "You are in First Room"};
-        Items = [];
-        Exits =
-         {
-            North = NoExit(None);
-            South = NoExit(None);
-            West = NoExit(None);
-            East = NoExit(None)
-            }
-     }    
+        Details: Details
+        Location: RoomId
+        Iventory: Item list
+    }
+
+type World = 
+    {
+        Rooms: Map<RoomId, RoomId>
+        Player: Player
+    }
+
+// ----------World starts.---------------
+
+//let firstRoom =
+//    {
+//        Id = 
+//        Details = {Name = "first Room "; Description = "You are in First Room"};
+//        Items = [];
+//        Exits =
+//         {
+//            North = NoExit(None);
+//            South = NoExit(None);
+//            West = NoExit(None);
+//            East = NoExit(None)
+//            }
+//     }    
+
+
+// -----------Logic for thegame -----------
+let de
