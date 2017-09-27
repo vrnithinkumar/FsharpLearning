560f5c1679799d7ac3dcd2516d2dec5817fa634a
VR <vrnithinkumar@gmail.com>
2017-09-27 12:52:53 +0530

    Adding the if else example

--- FSharpCore/ConsoleApp/ComputationExpressions.fs
+++ FSharpCore/ConsoleApp/ComputationExpressions.fs
@@ -1,3 +1,27 @@
 namespace ComputationExpressions
 module CompExp =
+    let map1 = [ ("1","One"); ("2","Two") ] |> Map.ofList
+    let map2 = [ ("A","Alice"); ("B","Bob") ] |> Map.ofList
+    let map3 = [ ("CA","California"); ("NY","New York") ] |> Map.ofList
     let TestValFromCompExp = 45
+
+    // Chains of "or else" tests.
+    let lookUpInMaps key =
+        match map1.TryFind key with
+        | Some res1 -> Some res1        
+        | None ->
+            printfn "Did not find in map1"
+            match map2.TryFind key with
+            | Some res2 -> Some res2
+            | None ->
+                printfn "Did not find in map2"
+                match map3.TryFind key with
+                | Some res3 -> Some res3
+                | None -> 
+                    printfn "Did not find in map3"
+                    None
+    
+    let callLookUp() = 
+        lookUpInMaps "A" |> printfn "Result for A is %A" 
+        lookUpInMaps "CA" |> printfn "Result for CA is %A" 
+        lookUpInMaps "X" |> printfn "Result for X is %A"
--- FSharpCore/ConsoleApp/Program.fs
+++ FSharpCore/ConsoleApp/Program.fs
@@ -5,5 +5,6 @@ open ComputationExpressions.CompExp
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#! \n Val is %A" TestValFromCompExp 
+    callLookUp()
     let x = Console.Read()
     0 // return an integer exit code
